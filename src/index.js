0abe48a239d876dec1a1d0c5885e3cc05e2e6411
KHOLKIN-V <kholkinva@yandex.ru>
2021-09-21 22:14:48 +0400

    HW-12 correct 1

--- src/index.js
+++ src/index.js
@@ -1,36 +1,34 @@
 import countryCards from '../templates/country-card.hbs';
-// import countryArray from '../templates/country-array.hbs';
+import countryArray from '../templates/country-array.hbs';
 import debounce from 'lodash.debounce';
+import { alert, defaultModules } from '@pnotify/core';
+import * as PNotifyMobile from '@pnotify/mobile';
+
+defaultModules.set(PNotifyMobile, {});
 
 const cardContainer = document.querySelector('.js-card-container');
 const searchCountry = document.querySelector('.js-search-form');
+const arrayContainer = document.querySelector('.js-array-container');
 
 searchCountry.addEventListener('input', debounce(onSearch, 500));
-// searchCountry.addEventListener('input', onSearch);
-
-// function onSearchInput(i) {
-//     i.preventDefault();
-
-//     if (10 >= i.length >= 2) {
-//         import countryArray from '../templates/country-array.hbs';
-//     } else if (i.length === 1) {
-//         import countryCards from '../templates/country-card.hbs';
-//         onSearch();
-//     } else if (i.length > 10) {
-
-//     }
-// }
 
 function onSearch (e) {
-    e.preventDefault();
-
-    const form = e.currentTarget;
-    const searchQuery = form.elements.query.value;
+    const form = e.target;
+    const searchQuery = form.value;
 
     fetchCountries(searchQuery)
-    .then(renderCountryCard)
-    .catch(fetchError)
-    .finally(() => form.reset());
+    .then(render => {
+        if (render.length >= 2 && render.length <= 10) {
+            return renderCountryArray(render);
+        } else if (render.length === 1) {
+            return renderCountryCard(render);
+        } else if (i.length > 10) {
+            alert({
+                text: 'Too many matches found. Please, enter a more specific query!'
+              });
+        }
+    })
+    .catch(fetchError);
 }
 
 function fetchCountries(countryId) {
@@ -39,11 +37,17 @@ return fetch(`https://restcountries.eu/rest/v2/name/${countryId}`)
 };
 
 function renderCountryCard (country) {
-    const markup = countryCards(country);
+    const markup = countryCards(country[0]);
     console.log(country);
     cardContainer.innerHTML = markup;
-}
+};
+
+function renderCountryArray (country) {
+    const markup = countryArray(country);
+    console.log(country);
+    arrayContainer.innerHTML = markup;
+};
 
 function fetchError(error) {
-    alert('r u srsly?!?!?');
+    alert('EEERRRRROOOOOORRRR');
 };
